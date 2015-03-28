18c0080feaabacede3240fd54f3b6177c3d4fcb9
ronyjacobson <ronyjacobson@gmail.com>
2015-03-28 12:08:37 +0300

    const

--- Ex1_CPP/person_t.h
+++ Ex1_CPP/person_t.h
@@ -9,7 +9,7 @@ public:
 	~Person_t();									//DTOR
 	Person_t(string& name, int age);				//Another CTOR
 	Person_t(const Person_t& pr);					//Copy CTOR
-	const Person_t& operator=(const Person_t& pr);	//Operator =
+	Person_t& operator=(const Person_t& pr);		//Operator =
 	bool operator==(const Person_t& pr);			//Operator ==
 
 	//Setters and Getters
