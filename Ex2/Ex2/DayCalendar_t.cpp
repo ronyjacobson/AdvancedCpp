27ba1a0a970aff751a671bebe16c3ac12e158586
Alon Grinshpoon <alon.grinshpoon@yahoo.com>
2015-04-22 13:50:31 +0300

    Delete Meeting 1

--- Ex2/Ex2/DayCalendar_t.cpp
+++ Ex2/Ex2/DayCalendar_t.cpp
@@ -19,9 +19,11 @@ void DayCalendar_t::insertMeeting(float endTime, float startTime){}
 
 void DayCalendar_t::deleteMeeting(const Meeting_t& meeting){}
 
-void DayCalendar_t::deleteMeeting(float startTime){}
+void DayCalendar_t::deleteMeeting(float startTime){
+	m_meetingsList.remove(searchForMeeting(startTime));
+}
 
-Meeting_t DayCalendar_t::searchForMeeting(float startTime){
+Meeting_t* const DayCalendar_t::searchForMeeting(float startTime){
 	for (list<Meeting_t*>::const_iterator ci = m_meetingsList.begin(); ci != m_meetingsList.end(); ++ci)
 		if (((Meeting_t&)*ci).getStartTime() == startTime)
 			return ((Meeting_t&)*ci);
