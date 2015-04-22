820f13b61458eb3360e56eb5236ca5c6d0b46000
ronyjacobson <ronyjacobson@gmail.com>
2015-04-22 13:00:05 +0300

    Calendar

--- Ex2/Ex2/DayCalendar_t.h
+++ Ex2/Ex2/DayCalendar_t.h
@@ -8,29 +8,36 @@ class DayCalendar_t
 public:
 	DayCalendar_t();										//CTOR
 	~DayCalendar_t();										//DTOR
+	DayCalendar_t(const DayCalendar_t& pr);					//Copy CTOR
 
-private:
-	Meeting_t m_firstMeeting;
-	Meeting_t m_lastMeeting;
-	int m_numOfMeetings;
-	
+	Meeting_t& getFirstMeeting() const {
+		return *m_firstMeeting;
+	}
+
+	Meeting_t& getLastMeeting() const {
+		return *m_lastMeeting;
+	}
+
+	void setFirstMeeting( Meeting_t* const meeting) {
+		m_firstMeeting = meeting;
+	}
+
+	void setLastMeeting(Meeting_t* const meeting) {
+		m_lastMeeting = meeting;
+	}
+
 	const DayCalendar_t& operator=(const DayCalendar_t& pr);	//Operator =
 	bool operator==(const DayCalendar_t& pr) const;				//Operator ==
-	DayCalendar_t(const DayCalendar_t& pr);						//Copy CTOR
-
-	// Functions
+	void insertMeeting(const Meeting_t& meeting);
+	void insertMeeting(float endTime, float startTime);
+	void deleteMeeting(const Meeting_t& meeting);
+	void deleteMeeting(float startTime);
+	void deleteMeeting(float endTime);
 	friend ostream& operator<< (ostream& os, const DayCalendar_t& calendar);
 
-	void incNumOfMeetings() {
-		m_numOfMeetings++;
-	};
 
-	int insertMeeting(const Meeting_t& meeting);
-	int insertMeeting(float endTime, float startTime);
-
-	int deleteMeeting(const Meeting_t& meeting);
-	int deleteMeeting(float startTime);
-	int deleteMeeting(float endTime);
-	
+private:
+	Meeting_t* m_firstMeeting;
+	Meeting_t* m_lastMeeting;
 };
 
