5198932ba6214f7f449e81229752628180a52550
ronyjacobson <ronyjacobson@gmail.com>
2015-04-22 13:25:04 +0300

    calendar with list

--- Ex2/Ex2/DayCalendar_t.cpp
+++ Ex2/Ex2/DayCalendar_t.cpp
@@ -2,7 +2,9 @@
 #include "DayCalendar_t.h"
 
 
-DayCalendar_t::DayCalendar_t(){}												//CTOR
+DayCalendar_t::DayCalendar_t(){ 												//CTOR
+
+}
 DayCalendar_t::~DayCalendar_t(){}												//DTOR
 DayCalendar_t::DayCalendar_t(const DayCalendar_t& pr){}							//Copy CTOR
 const DayCalendar_t& DayCalendar_t::operator=(const DayCalendar_t& pr){}		//Operator =
@@ -12,4 +14,6 @@ void DayCalendar_t::insertMeeting(float endTime, float startTime){}
 void DayCalendar_t::deleteMeeting(const Meeting_t& meeting){}
 void DayCalendar_t::deleteMeeting(float startTime){}
 void DayCalendar_t::deleteMeeting(float endTime){}
-ostream& operator<< (ostream& os, const DayCalendar_t& calendar){}
+ostream& operator<< (ostream& os, const DayCalendar_t& calendar){
+	return os;
+}
--- Ex2/Ex2/DayCalendar_t.h
+++ Ex2/Ex2/DayCalendar_t.h
@@ -1,5 +1,6 @@
 
 #include "Meeting_t.h"
+#include <List>
 #include <iostream>
 using namespace std;
 
@@ -9,22 +10,6 @@ public:
 	DayCalendar_t();										//CTOR
 	~DayCalendar_t();										//DTOR
 	DayCalendar_t(const DayCalendar_t& pr);					//Copy CTOR
-
-	Meeting_t& getFirstMeeting() const {
-		return *m_firstMeeting;
-	}
-
-	Meeting_t& getLastMeeting() const {
-		return *m_lastMeeting;
-	}
-
-	void setFirstMeeting( Meeting_t* const meeting) {
-		m_firstMeeting = meeting;
-	}
-
-	void setLastMeeting(Meeting_t* const meeting) {
-		m_lastMeeting = meeting;
-	}
 
 	const DayCalendar_t& operator=(const DayCalendar_t& pr);	//Operator =
 	bool operator==(const DayCalendar_t& pr) const;				//Operator ==
@@ -44,8 +29,7 @@ public:
 	};
 
 private:
-	Meeting_t* m_firstMeeting;
-	Meeting_t* m_lastMeeting;
+	list<Meeting_t*> m_meetingsList;
 };
 
 
