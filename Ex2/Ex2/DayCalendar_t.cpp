4ed39fd0bd07d9ae00baf9949c94f8888c6c0023
Alon Grinshpoon <alon.grinshpoon@yahoo.com>
2015-04-22 13:29:57 +0300

    Small updates

--- Ex2/Ex2/DayCalendar_t.cpp
+++ Ex2/Ex2/DayCalendar_t.cpp
@@ -8,12 +8,13 @@ DayCalendar_t::DayCalendar_t(){ 												//CTOR
 DayCalendar_t::~DayCalendar_t(){}												//DTOR
 DayCalendar_t::DayCalendar_t(const DayCalendar_t& pr){}							//Copy CTOR
 const DayCalendar_t& DayCalendar_t::operator=(const DayCalendar_t& pr){}		//Operator =
-bool DayCalendar_t::operator==(const DayCalendar_t& pr) const{}				//Operator ==
+bool DayCalendar_t::operator==(const DayCalendar_t& pr) const{}					//Operator ==
 void DayCalendar_t::insertMeeting(const Meeting_t& meeting){}
 void DayCalendar_t::insertMeeting(float endTime, float startTime){}
 void DayCalendar_t::deleteMeeting(const Meeting_t& meeting){}
 void DayCalendar_t::deleteMeeting(float startTime){}
-void DayCalendar_t::deleteMeeting(float endTime){}
+void DayCalendar_t::searchForMeeting(float startTime){}
+
 ostream& operator<< (ostream& os, const DayCalendar_t& calendar){
 	return os;
 }
