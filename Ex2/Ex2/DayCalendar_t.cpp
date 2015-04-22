f893aa5a13435b8015cc3cc85b22cda948f1b9a5
ronyjacobson <ronyjacobson@gmail.com>
2015-04-22 13:04:51 +0300

    function imp

--- Ex2/Ex2/DayCalendar_t.cpp
+++ Ex2/Ex2/DayCalendar_t.cpp
@@ -2,36 +2,14 @@
 #include "DayCalendar_t.h"
 
 
-DayCalendar_t::DayCalendar_t()
-{
-}
-
-
-
-DayCalendar_t::DayCalendar_t(const DayCalendar_t& cal)							// Copy CTOR
-{
-}
-
-
-DayCalendar_t::~DayCalendar_t()													//DTOR
-{
-}
-
-const DayCalendar_t& DayCalendar_t::operator=(const DayCalendar_t& cal)			// Operator =
-{
-}
-
-bool DayCalendar_t::operator==(const DayCalendar_t& cal)	const				// Operator ==
-{
-}
-
-void DayCalendar_t::insertMeeting(const Meeting_t& meeting){
-};
-void DayCalendar_t::insertMeeting(float endTime, float startTime){
-};
-void DayCalendar_t::deleteMeeting(const Meeting_t& meeting){
-};
-void DayCalendar_t::deleteMeeting(float startTime){
-};
-void DayCalendar_t::deleteMeeting(float endTime){
-};
+DayCalendar_t::DayCalendar_t(){}												//CTOR
+DayCalendar_t::~DayCalendar_t(){}												//DTOR
+DayCalendar_t::DayCalendar_t(const DayCalendar_t& pr){}							//Copy CTOR
+const DayCalendar_t& DayCalendar_t::operator=(const DayCalendar_t& pr){}		//Operator =
+bool DayCalendar_t::operator==(const DayCalendar_t& pr) const{}				//Operator ==
+void DayCalendar_t::insertMeeting(const Meeting_t& meeting){}
+void DayCalendar_t::insertMeeting(float endTime, float startTime){}
+void DayCalendar_t::deleteMeeting(const Meeting_t& meeting){}
+void DayCalendar_t::deleteMeeting(float startTime){}
+void DayCalendar_t::deleteMeeting(float endTime){}
+friend ostream& operator<< (ostream& os, const DayCalendar_t& calendar){}
