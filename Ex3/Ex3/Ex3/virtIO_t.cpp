336bf622babf411be4b2333a69e611886c147b96
Alon Grinshpoon <alon.grinshpoon@yahoo.com>
2015-05-08 16:58:55 +0300

    virtIO in the works

--- Ex3/Ex3/Ex3/virtIO_t.cpp
+++ Ex3/Ex3/Ex3/virtIO_t.cpp
@@ -2,22 +2,30 @@
 
 // CTOR
 virtIO_t::virtIO_t() {
-	m_path = "./newFile.txt";
-	m_mode = std::ios_base::in;
-	m_fileStream.open(m_path, m_mode);
+	m_file = 0;
+	m_path = "";
+	m_mode = 0;
+	m_status = bad_access_e;
+	m_lenght = 0;
 }		
 
 // DTOR
 virtIO_t::~virtIO_t() {
-	m_fileStream.close();
+	fclose(m_file);
 }									
 
 // CTOR By path and mode
-virtIO_t::virtIO_t(string path, ios_base::openmode mode)
+virtIO_t::virtIO_t(const char * path, const char * mode)
 {
 	m_path = path;
 	m_mode = mode;
-	m_fileStream.open(path, mode);
+	m_file = fopen(m_path, m_mode);
+
+	// Check File
+	if (m_file == NULL)
+		m_status = cant_open_file_e;
+	else
+		m_status = ok_e;
 }
 
 // Operator ==
@@ -26,24 +34,16 @@ bool virtIO_t::operator==(const virtIO_t& virtio) const{
 }
 
 // Getters
-string virtIO_t::getPath(){
+string virtIO_t::getPath() const{
 	return m_path;
 }
 
-ios_base::openmode virtIO_t::getMode(){
+ios_base::openmode virtIO_t::getAccess() const{
 	return m_mode;
 }
 
-int virtIO_t::getFileLength(){
+int virtIO_t::getFileLength() const{
 	return m_lenght;
-}
-
-// IO Operations
-size_t virtIO_t::read(void* ptr, size_t size, size_t count){
-
-}
-size_t virtIO_t::write(const void* ptr, size_t size, size_t count){
-
 }
 
 // IO Operations
--- Ex3/Ex3/Ex3/virtIO_t.h
+++ Ex3/Ex3/Ex3/virtIO_t.h
@@ -6,56 +6,73 @@
 
 using namespace std;
 
-enum FileStatus { ok_e, cant_open_file_e, bad_access_e, writeErr_e, readErr_e };
-
 class virtIO_t
 {
 
 public:
 	virtIO_t();											// CTOR
-	virtIO_t(string path, ios_base::openmode mode);		// CTOR By Path And Mode
+	virtIO_t(const char * path, const char * mode);		// CTOR By Path And Mode
 	~virtIO_t();										// DTOR
-	virtual bool operator==(const virtIO_t& virtio) const;
-	virtual string getPath();
-	virtual ios_base::openmode getMode();
-	virtual int getFileLength();
+
+	// File Status Enum
+	enum fileStatus { ok_e, cant_open_file_e, bad_access_e, writeErr_e, readErr_e };
+
+	// Getters
+	virtual const char * getPath() const;
+	virtual const char * getAccess() const;
+	virtual long getFileLength() const;
+
+	// Operations and operators
 	virtual size_t read(void* ptr, size_t size, size_t count);
 	virtual size_t write(const void* ptr, size_t size, size_t count);
-	friend virtual ostream operator,(ostream &out, int& len);
-	friend virtual istream& operator,(istream &in, const int& len);
+	virtual bool operator==(const virtIO_t& virtio) const;
+	virtual virtIO_t& operator,(int len);
+
+	// operator<< overloads
+	virtual  virtIO_t& operator<<(const char& buffer) = 0;
+	virtual  virtIO_t& operator<<(const unsigned char& buffer) = 0;
+	virtual  virtIO_t& operator<<(const short& buffer) = 0;
+	virtual  virtIO_t& operator<<(const unsigned short& buffer) = 0;
+	virtual  virtIO_t& operator<<(const int& buffer) = 0;
+	virtual  virtIO_t& operator<<(const unsigned int& buffer) = 0;
+	virtual  virtIO_t& operator<<(const long& buffer) = 0;
+	virtual  virtIO_t& operator<<(const unsigned long& buffer) = 0;
+	virtual  virtIO_t& operator<<(const float& buffer) = 0;
+	virtual  virtIO_t& operator<<(const double& buffer) = 0;
 	
-	// operator<< overloads:
-	friend virtual ostream operator<<(ostream &out, void* buffer);
-	friend virtual ostream operator<<(ostream &out, char input);
-	friend virtual ostream operator<<(ostream &out, unsigned char input);
-	friend virtual ostream operator<<(ostream &out, short input);
-	friend virtual ostream operator<<(ostream &out, unsigned short input);
-	friend virtual ostream operator<<(ostream &out, int input);
-	friend virtual ostream operator<<(ostream &out, unsigned int input);
-	friend virtual ostream operator<<(ostream &out, long input);
-	friend virtual ostream operator<<(ostream &out, unsigned long input);
-	friend virtual ostream operator<<(ostream &out, float input);
-	friend virtual ostream operator<<(ostream &out, double input);
-	// operator>> overloads:
-	friend virtual istream& operator>>(istream &in, const void* buffer);
-	friend virtual istream& operator>>(istream &in, char input);
-	friend virtual istream& operator>>(istream &in, unsigned char input);
-	friend virtual istream& operator>>(istream &in, short input);
-	friend virtual istream& operator>>(istream &in, unsigned short input);
-	friend virtual istream& operator>>(istream &in, int input);
-	friend virtual istream& operator>>(istream &in, unsigned int input);
-	friend virtual istream& operator>>(istream &in, long input);
-	friend virtual istream& operator>>(istream &in, unsigned long input);
-	friend virtual istream& operator>>(istream &in, float input);
-	friend virtual istream& operator>>(istream &in, double input);
+	virtual virtIO_t& operator<<(const char* buffer) = 0;
+	virtIO_t& operator<<(const void* buffer);
+
+	// operator>> overloads
+	virtual  virtIO_t& operator>>(char& buffer) = 0;
+	virtual  virtIO_t& operator>>(unsigned char& buffer) = 0;
+	virtual  virtIO_t& operator>>(short& buffer) = 0;
+	virtual  virtIO_t& operator>>(unsigned short& buffer) = 0;
+	virtual  virtIO_t& operator>>(int& buffer) = 0;
+	virtual  virtIO_t& operator>>(unsigned int& buffer) = 0;
+	virtual  virtIO_t& operator>>(long& buffer) = 0;
+	virtual  virtIO_t& operator>>(unsigned long& buffer) = 0;
+	virtual  virtIO_t& operator>>(float& buffer) = 0;
+	virtual  virtIO_t& operator>>(double& buffer) = 0;
+	
+	virtual virtIO_t& operator>>(char* buffer) = 0;
+	virtIO_t& operator>>(void* buffer);
+
+	// Status getter and setter
+	inline fileStatus getStatus()const{
+		return m_status;
+	}
+
+	inline void set(fileStatus status){
+		m_status = status;
+	}
 
 protected:
-	fstream				m_fileStream;
-	string				m_path;
-	ios_base::openmode	m_mode;
-	unsigned int		m_inputOffset;
-	unsigned int		m_outputOffset;
-	int					m_lenght;
+	FILE *				m_file;
+	const char *		m_path;
+	const char *		m_mode;
+	fileStatus			m_status;
+	long				m_lenght;
 };
 
 #endif
