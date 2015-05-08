05de17e51d62b3a821a0ee3fdefafc48aab07302
ronyjacobson <ronyjacobson@gmail.com>
2015-05-08 17:16:06 +0300

    commitush

---
+++ Ex3/Ex3/Ex3/binIO.h
@@ -0,0 +1,73 @@
+#include "virtIO_t.h"
+
+class binIO_t : virtIO_t{
+public:
+
+	binIO_t();			//default CTOR
+	binIO_t(const char *pathname, const char* mode); //name CTOR
+	~binIO_t();			//DTOR
+	virtual bool setMode(const char *mode);
+
+	
+	
+
+	// operator<< overloads:
+	using virtIO_t::operator<<;
+	virtual virtIO_t& operator<<(const unsigned char& buf);
+	virtual virtIO_t& operator<<(const char& buf);
+	virtual virtIO_t& operator<<(const unsigned short& buf);
+	virtual virtIO_t& operator<<(const short& buf);
+	virtual virtIO_t& operator<<(const unsigned int& buf);
+	virtual virtIO_t& operator<<(const int& buf);
+	virtual virtIO_t& operator<<(const unsigned long& buf);
+	virtual virtIO_t& operator<<(const long& buf);
+	virtual virtIO_t& operator<<(const double& buf);
+	virtual virtIO_t& operator<<(const float& buf);
+	virtual virtIO_t& operator<<(const char* buf);
+
+	// operator>> overloads:
+	using virtIO_t::operator>>;
+	virtual virtIO_t& operator>>(unsigned char& buf);
+	virtual virtIO_t& operator>>(char& buf);
+	virtual virtIO_t& operator>>(unsigned short& buf);
+	virtual virtIO_t& operator>>(short& buf);
+	virtual virtIO_t& operator>>(unsigned int& buf);
+	virtual virtIO_t& operator>>(int& buf);
+	virtual virtIO_t& operator>>(unsigned long& buf);
+	virtual virtIO_t& operator>>(long& buf);
+	virtual virtIO_t& operator>>(double& buf);
+	virtual virtIO_t& operator>>(float& buf);
+	virtual virtIO_t& operator>>(char* buf);
+
+
+private:
+
+	// Templated write function for output (operator <<)
+	template <class T> binIO_t& writeTemplate(const T& type){
+		if (this->good()) {
+			if (fwrite(&type, sizeof(T), 1, mFile) < 1) {
+				mStatus = virtIO_t::writeErr;
+			}
+		}
+		return *this;
+	}
+
+	// Templated read function for input(operator >>)
+	template <class T>  binIO_t& readTemplate(T& type){
+		if (this->good()) {
+			fread(&type, sizeof(T), 1, mFile);
+			if (feof(mFile)) {
+				mStatus = virtIO_t::readErr_e;
+			} else {
+				if (ferror(mFile)) {
+					mStatus = virtIO_t::bad_access_e;
+				}
+			}
+		}
+		return * this;
+	}
+
+	binIO_t(const binIO_t& file);				   //copy CTOR
+	const binIO_t& operator=(const binIO_t& file); //operator =
+	
+};
