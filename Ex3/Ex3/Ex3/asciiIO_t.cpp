58449938de42a05e01b5aab27574077cbb41916b
Capibara- <chubi123@gmail.com>
2015-05-08 15:52:50 +0300

    Vcxproj only commit.

--- Ex3/Ex3/Ex3/asciiIO_t.cpp
+++ Ex3/Ex3/Ex3/asciiIO_t.cpp
@@ -2,9 +2,18 @@
 
 using namespace std;
 
-class asciiIO_t
+class asciiIO_t : virtIO_t
 {
 public:
+	asciiIO_t() : virtIO_t()
+	{
+		// Call parent constructor.
+	}
+
+	asciiIO_t(string path, ios_base::openmode mode) : virtIO_t(path, mode)
+	{
+		// Call parent constructor.
+	}
 
 private:
 
