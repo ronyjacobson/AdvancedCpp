50318ddea2d88b979aecc08a1c47e91350d7ffc7
Capibara- <chubi123@gmail.com>
2015-05-08 17:38:49 +0300

    Added << and >> implementation.

--- Ex3/Ex3/Ex3/asciiIO_t.cpp
+++ Ex3/Ex3/Ex3/asciiIO_t.cpp
@@ -1,42 +1,129 @@
-#include "aciiIO_t .h"
+#include "asciiIO_t.h"
 
 using namespace std;
 
-class asciiIO_t : virtIO_t
+asciiIO_t::asciiIO_t()
 {
-public:
-	asciiIO_t() : virtIO_t()
-	{
-		// No work to do, call parent constructor.
-	}
+	// No work to do, call parent constructor.
+}
 
-	asciiIO_t(string path, ios_base::openmode mode) : virtIO_t(path, mode)
-	{
-		// No work to do, call parent constructor.
-	}
-	~asciiIO_t()
-	{
-		// No work to do, call parent destructor.
-	}
+asciiIO_t::asciiIO_t(char * path, char * mode) : virtIO_t(path, mode)
+{
+	// No work to do, call parent constructor.
+}
+asciiIO_t::~asciiIO_t()
+{
+	// No work to do, call parent destructor.
+}
 
-	virtual size_t read(void* ptr, size_t size, size_t count)
-	{
-		if (m_fileStream.good() && m_fileStream.is_open())
-		{
-			char* buffer = (char*)ptr;
-			m_fileStream.read(buffer, count*size);
-		}
-	}
+size_t asciiIO_t::read(void* ptr, size_t size, size_t count)
+{
 
-	virtual size_t write(const void* ptr, size_t size, size_t count)
-	{
-		if (m_fileStream.good() && m_fileStream.is_open())
-		{
-			const char* buffer = (char*)ptr;
-			m_fileStream.write(buffer, count*size);
-		}
-	}
+}
 
-private:
+size_t asciiIO_t::write(const void* ptr, size_t size, size_t count)
+{
 
-};
+}
+
+virtIO_t& asciiIO_t::operator>>(unsigned char& buf)
+{
+	return readTemplate<unsigned char>(buf, "%c");
+}
+
+virtIO_t& asciiIO_t::operator>>(char& buf)
+{
+	return readTemplate<char>(buf, "%hhd");
+}
+
+virtIO_t& asciiIO_t::operator>>(unsigned short& buf)
+{
+	return readTemplate<unsigned short>(buf, "%hu");
+}
+
+virtIO_t& asciiIO_t::operator>>(short& buf)
+{
+	return readTemplate<short>(buf, "%hd");
+}
+
+virtIO_t& asciiIO_t::operator>>(unsigned int& buf)
+{
+	return readTemplate<unsigned int>(buf, "%u");
+}
+
+virtIO_t& asciiIO_t::operator>>(int& buf)
+{
+	return readTemplate<int>(buf, "%d");
+}
+
+virtIO_t& asciiIO_t::operator>>(unsigned long& buf)
+{
+	return readTemplate<unsigned long>(buf, "%lu");
+}
+
+virtIO_t& asciiIO_t::operator>>(long& buf)
+{
+	return readTemplate<long>(buf, "%ld");
+}
+
+virtIO_t& asciiIO_t::operator>>(float& buf)
+{
+	return readTemplate<float>(buf, "%f");
+}
+
+virtIO_t& asciiIO_t::operator>>(double& buf)
+{
+	return readTemplate<double>(buf, "%lf");
+}
+
+
+
+virtIO_t& asciiIO_t::operator<<(const unsigned char& buf)
+{
+	return writeTemplate<unsigned char>(buf, "%c");
+}
+
+virtIO_t& asciiIO_t::operator<<(const char& buf)
+{
+	return writeTemplate<char>(buf, "%hhd");
+}
+
+virtIO_t& asciiIO_t::operator<<(const unsigned short& buf)
+{
+	return writeTemplate<unsigned short>(buf, "%hu");
+}
+
+virtIO_t& asciiIO_t::operator<<(const short& buf)
+{
+	return writeTemplate<short>(buf, "%hd");
+}
+
+virtIO_t& asciiIO_t::operator<<(const unsigned int& buf)
+{
+	return writeTemplate<unsigned int>(buf, "%u");
+}
+
+virtIO_t& asciiIO_t::operator<<(const int& buf)
+{
+	return writeTemplate<int>(buf, "%d");
+}
+
+virtIO_t& asciiIO_t::operator<<(const unsigned long& buf)
+{
+	return writeTemplate<unsigned long>(buf, "%lu");
+}
+
+virtIO_t& asciiIO_t::operator<<(const long& buf)
+{
+	return writeTemplate<long>(buf, "%ld");
+}
+
+virtIO_t& asciiIO_t::operator<<(const float& buf)
+{
+	return writeTemplate<float>(buf, "%f");
+}
+
+virtIO_t& asciiIO_t::operator<<(const double& buf)
+{
+	return writeTemplate<double>(buf, "%lf");
+}
---
+++ Ex3/Ex3/Ex3/asciiIO_t.h
@@ -0,0 +1,73 @@
+#ifndef ASCIIIO_T_H
+#define ASCIIIO_T_H
+
+#include "virtIO_t.h"
+
+using namespace std;
+
+class asciiIO_t : virtIO_t
+{
+public:
+	asciiIO_t();
+	asciiIO_t(char * path, char * mode);
+	~asciiIO_t();
+	virtual size_t read(void* ptr, size_t size, size_t count);
+	virtual size_t write(const void* ptr, size_t size, size_t count);
+	virtual  virtIO_t& operator<<(const unsigned char& buf);
+
+	virtual virtIO_t& operator>>(unsigned char& buf);
+	virtual virtIO_t& operator>>(char& buf);
+	virtual virtIO_t& operator>>(unsigned short& buf);
+	virtual virtIO_t& operator>>(short& buf);
+	virtual virtIO_t& operator>>(unsigned int& buf);
+	virtual virtIO_t& operator>>(int& buf);
+	virtual virtIO_t& operator>>(unsigned long& buf);
+	virtual virtIO_t& operator>>(long& buf);
+	virtual virtIO_t& operator>>(float& buf);
+	virtual virtIO_t& operator>>(double& buf);
+
+	virtual virtIO_t& operator<<(const unsigned char& buf);
+	virtual virtIO_t& operator<<(const char& buf);
+	virtual virtIO_t& operator<<(const unsigned short& buf);
+	virtual virtIO_t& operator<<(const short& buf);
+	virtual virtIO_t& operator<<(const unsigned int& buf);
+	virtual virtIO_t& operator<<(const int& buf);
+	virtual virtIO_t& operator<<(const const unsigned long& buf);
+	virtual virtIO_t& operator<<(const long& buf);
+	virtual virtIO_t& operator<<(const float& buf);
+	virtual virtIO_t& operator<<(const double& buf);
+	using virtIO_t::operator<<;
+	using virtIO_t::operator >>;
+
+private:
+	asciiIO_t(const asciiIO_t& obj); // Copy CTOR
+	const asciiIO_t& operator=(const asciiIO_t& obj); // Operator=
+
+
+	template <class T> asciiIO_t& writeTemplate(const T& buf, const char* format)
+	{
+		if (this->getStatus() == virtIO_t::ok_e){
+			if (fprintf(_file, format, buf) < 0)
+			{
+				_status = virtIO_t::writeErr_e;
+			}
+			//add blanks between fields
+			fprintf(_file, "%c", ' ');
+		}
+		return *this;
+	}
+
+	template <class T> asciiIO_t& readTemplate(T& buf, const char* format)
+	{
+		if (this->getStatus() == virtIO_t::ok_e){
+			if (fscanf(_file, format, &buf) == EOF)
+			{
+				_status = virtIO_t::readErr_e;
+			}
+		}
+		return *this;
+	}
+};
+
+
+#endif
