04053ff29b14db689a93553dcf674e6045b27a42
Alon Grinshpoon <alon.grinshpoon@yahoo.com>
2015-05-08 16:22:57 +0300

    Revert

--- Ex3/Ex3/Ex3/virtIO_t.cpp
+++ Ex3/Ex3/Ex3/virtIO_t.cpp
@@ -1,59 +1,55 @@
 #include "virtIO_t.h"
 
-class virtIO_t
+// CTOR
+virtIO_t::virtIO_t() {
+	m_path = "./newFile.txt";
+	m_mode = std::ios_base::in;
+	m_fileStream.open(m_path, m_mode);
+}		
+
+// DTOR
+virtIO_t::~virtIO_t() {
+	m_fileStream.close();
+}									
+
+// CTOR By path and mode
+virtIO_t::virtIO_t(string path, ios_base::openmode mode)
 {
-public:
-	// CTOR
-	virtIO_t::virtIO_t() {
-		m_path = "./newFile.txt";
-		m_mode = std::ios_base::in;
-		m_fileStream.open(m_path, m_mode);
-	}
+	m_path = path;
+	m_mode = mode;
+	m_fileStream.open(path, mode);
+}
 
-	// DTOR
-	virtIO_t::~virtIO_t() {
-		m_fileStream.close();
-	}
+// Operator ==
+bool virtIO_t::operator==(const virtIO_t& virtio) const{
+	return (m_path == virtio.m_path) && (m_mode == virtio.m_mode);
+}
 
-	// CTOR By path and mode
-	virtIO_t::virtIO_t(string path, ios_base::openmode mode)
-	{
-		m_path = path;
-		m_mode = mode;
-		m_fileStream.open(path, mode);
-	}
+// Getters
+string virtIO_t::getPath(){
+	return m_path;
+}
 
-	// Operator ==
-	bool virtIO_t::operator==(const virtIO_t& virtio) const{
-		return (m_path == virtio.m_path) && (m_mode == virtio.m_mode);
-	}
+ios_base::openmode virtIO_t::getMode(){
+	return m_mode;
+}
 
-	// Getters
-	string virtIO_t::getPath(){
-		return m_path;
-	}
+int virtIO_t::getFileLength(){
+	return m_lenght;
+}
 
-	ios_base::openmode virtIO_t::getMode(){
-		return m_mode;
-	}
+// IO Operations
+size_t virtIO_t::read(void* ptr, size_t size, size_t count){
 
-	int virtIO_t::getFileLength(){
-		return m_lenght;
-	}
+}
+size_t virtIO_t::write(const void* ptr, size_t size, size_t count){
 
-	// IO Operations
-	size_t virtIO_t::read(void* ptr, size_t size, size_t count){
+}
 
-	}
-	size_t virtIO_t::write(const void* ptr, size_t size, size_t count){
+// IO Operations
+ostream virtIO_t::operator,(ostream &out, int& len){
 
-	}
+}
+istream& virtIO_t::operator,(istream &in, const int& len){
 
-	// IO Operations
-	ostream virtIO_t::operator,(ostream &out, int& len){
-
-	}
-	istream& virtIO_t::operator,(istream &in, const int& len){
-
-	}
 }
